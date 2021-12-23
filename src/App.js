b97151c05e9bdf963bdacdba84c5bc60878e5073
Liran-tal <taliran44@gmail.com>
2021-12-23 17:40:38 +0200

    main connection check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Main from './components/Main/main';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      WEEKEND 9 - MATCH FINDING
+      <Main />
     </div>
   );
 }
